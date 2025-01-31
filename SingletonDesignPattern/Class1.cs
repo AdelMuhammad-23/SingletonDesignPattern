a54dfbdf87707f2fcefa4fe85a6202633b5b59a2
Adel Muhammad <141270987+AdelMohamed23@users.noreply.github.com>
2025-01-31 20:14:03 +0200

    example

--- SingletonDesignPattern/Class1.cs
+++ SingletonDesignPattern/Class1.cs
@@ -1,7 +1,41 @@
-﻿namespace SingletonDesignPattern
+﻿class Program
 {
-    public class Class1
+    static void Main()
     {
+        Logger logger1 = Logger.GetInstance();
+        Logger logger2 = Logger.GetInstance();
 
+        logger1.Log("Hello from Singleton!");
+
+        Console.WriteLine(Object.ReferenceEquals(logger1, logger2));
     }
 }
+
+
+
+
+public class Logger
+{
+    private static Logger _instance;
+
+    private Logger()
+    {
+        Console.WriteLine("Logger Instance Created!");
+    }
+
+    public static Logger GetInstance()
+    {
+        if (_instance == null)
+        {
+            _instance = new Logger();
+        }
+        return _instance;
+    }
+
+    public void Log(string message)
+    {
+        Console.WriteLine($"Log: {message}");
+    }
+}
+
+
